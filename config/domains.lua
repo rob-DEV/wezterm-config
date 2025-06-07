9874ff15aab7be5c56e5a2b4a33d37893dabcc10
Robert <masseymann@outlook.com>
2025-06-07 14:22:00 +0100

    Change shell to zsh

--- config/domains.lua
+++ config/domains.lua
@@ -7,7 +7,7 @@ return {
          name = 'wsl.ssh',
          remote_address = 'localhost',
          multiplexing = 'None',
-         default_prog = { 'fish', '-l' },
+         default_prog = { 'zsh', '-l' },
          assume_shell = 'Posix'
       }
    },
@@ -18,11 +18,11 @@ return {
    -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
    wsl_domains = {
       {
-         name = 'WSL:Ubuntu',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish', '-l' },
+         name = 'fedora',
+         distribution = 'Fedora',
+         username = 'robert',
+         default_cwd = '/home/robert',
+         default_prog = { 'zsh', '-l' },
       },
    },
 }
--- wezterm.lua
+++ wezterm.lua
@@ -15,6 +15,6 @@ return Config:init()
    :append(require('config.appearance'))
    :append(require('config.bindings'))
    :append(require('config.domains'))
-   :append(require('config.fonts'))
+   -- :append(require('config.fonts'))
    :append(require('config.general'))
    :append(require('config.launch')).options
